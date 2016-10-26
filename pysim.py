67720814e6b885662b164254c577f8f25b7d3e86
pranav1001 <pranavsankhe40@gmail.com>
2016-10-26 18:48:49 +0530

    Acheived correct simulation of rectangular slab(xy plane) with variable refractive index

--- pysim.py
+++ pysim.py
@@ -5,43 +5,53 @@ import sys
 
 sys.setrecursionlimit(1500)
 
-n = 1000
-vec_x = np.arange(n)
+no_of_iterations = 1000
+vec_x = np.arange(no_of_iterations)
+in_angle  = np.pi*25.0/180.0
+in_height = 10 
+vec_y = np.zeros(no_of_iterations)
 #vec_y = np.arange(n)
 
-r_index = np.ones(n)
+r_index = np.ones(no_of_iterations)
 
-for i in range(1000) :
-	r_index[i] = r_index[i]*2 
+for i in range(1,1000) :
+	r_index[i] = r_index[i]/(1+ np.power(i,3) )
+	print(r_index[i])
+	
 
-in_angle  = np.pi/4
-in_height = 10 
-vec_y = np.array([])
+#print(r_index)		
+
+
+r_index[0] = 1
+
 
 def ang_of_incidence(n):
     if n == 1:
-    	incident_ang = r_index[0]/r_index[1] 
+    	incident_ang = np.sin(in_angle)*(r_index[0]/r_index[1]) 
         return incident_ang
     else:
     	incident_ang = (r_index[n-1]/r_index[n])*ang_of_incidence(n-1) 
         return  incident_ang
 
 def path_eq(n):
-	path_of_ray = np.array([]) 
+	
 	if n  == 0:
-		path_of_ray[0] = in_height
-		return path_of_ray
+		vec_y[n] = in_height
+		return vec_y
+		
 	else:
-		path_of_ray[n] = path_eq(n-1) + np.tan(np.arcsin(np.sin(in_angle)*ang_of_incidence(n))) 
-		return path_of_ray
+		vec_y[n] = path_eq(n-1)[n-1] + np.tan(np.arcsin(ang_of_incidence(n))) 
+		return vec_y
 
-vec_y = path_eq(n)
+def get_slope(n):
+    print('slope : ' , (180/np.pi)*np.arcsin(ang_of_incidence(n)))
 
-plt.plot(vec_x, vec_y)
+get_slope(50)
+
+
+plt.plot(vec_x, path_eq(no_of_iterations-1))
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
-
-
 
 
