2b09bf7aaa1c110d08eac53263bb20e908b094db
pranav1001 <pranavsankhe40@gmail.com>
2016-10-26 18:59:55 +0530

    simulation done in x-y plane with refractive index as a function of x

--- pysim.py
+++ pysim.py
@@ -5,27 +5,21 @@ import sys
 
 sys.setrecursionlimit(1500)
 
-no_of_iterations = 1000
+no_of_iterations = 1000             
 vec_x = np.arange(no_of_iterations)
-in_angle  = np.pi*25.0/180.0
-in_height = 10 
+in_angle  = np.pi*25.0/180.0                                 #incident angle
+in_height = 10                             #The value of y co-ordinate where the ray hits the medium
 vec_y = np.zeros(no_of_iterations)
-#vec_y = np.arange(n)
-
 r_index = np.ones(no_of_iterations)
 
 for i in range(1,1000) :
-	r_index[i] = r_index[i]/(1+ np.power(i,3) )
-	print(r_index[i])
+	r_index[i] = r_index[i]/(1+ float(i)/1000.0)      #set the refractive index function here. 
 	
-
-#print(r_index)		
-
-
-r_index[0] = 1
+	
+r_index[0] = 1                               #refractive index of outer medium 
 
 
-def ang_of_incidence(n):
+def ang_of_incidence(n):                     #decides the angle of refraction for the next iteration step 
     if n == 1:
     	incident_ang = np.sin(in_angle)*(r_index[0]/r_index[1]) 
         return incident_ang
@@ -33,7 +27,7 @@ def ang_of_incidence(n):
     	incident_ang = (r_index[n-1]/r_index[n])*ang_of_incidence(n-1) 
         return  incident_ang
 
-def path_eq(n):
+def path_eq(n):                               #trajectory of ray 
 	
 	if n  == 0:
 		vec_y[n] = in_height
@@ -43,7 +37,7 @@ def path_eq(n):
 		vec_y[n] = path_eq(n-1)[n-1] + np.tan(np.arcsin(ang_of_incidence(n))) 
 		return vec_y
 
-def get_slope(n):
+def get_slope(n):                              
     print('slope : ' , (180/np.pi)*np.arcsin(ang_of_incidence(n)))
 
 get_slope(50)
