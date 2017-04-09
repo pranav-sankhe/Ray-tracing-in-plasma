7a41a9c2e582db9a5a6f6248671b60f79f995cd4
Pranav Sankhe <pranavsankhe40@gmail.com>
2017-04-09 07:39:21 +0530

    Simulation tested. Sucessfull

--- python scripts/script.py
+++ python scripts/script.py
@@ -10,45 +10,59 @@ from scipy.integrate import ode
 from scipy import integrate
 from scipy.optimize import fsolve
 
-freq = 18#np.power(10,6)
+freq = 10#np.power(10,6)
 
 def sqr_ref_index(r,freq):
-	val = 1 - 38.27/np.power(r,6)
+	val = 1 - (12400/(freq*freq))/np.power(r,6)
 	return val
 
-def singularity(r_list):
+def singularity(r_list,ray_param):
 	for r in r_list:
 		val = sqr_ref_index(r,freq)*r*r - ray_param*ray_param	
 		if val > 0:
 			return r	
 
-def DthetaDr(r):
+def DthetaDr(r,ray_param):
 	val = -ray_param/r
 	val1 = sqr_ref_index(r,freq)*r*r - ray_param*ray_param	
 	val1 = np.sqrt(val1)
 	val = val/val1 
 	return val 
 
-ray_param = 3
+def simulation(ray_param):
+	r_list = np.linspace(1,5,10000)
+	rCritical = singularity(r_list,ray_param)
 
-r_list = np.linspace(1,5,10000)
-rCritical = singularity(r_list)
-thetaC = integrate.quad(lambda x: DthetaDr(x) , rCritical, 5)[0]
+	thetaC = integrate.quad(lambda x: DthetaDr(x,ray_param) , rCritical, 5)[0]
 
-radial_dist = np.linspace(rCritical,5,10000)
+	radial_dist_1 = np.linspace(rCritical,5,10000)
 
-sol1 = []
-for i in range(len(radial_dist)):
-	result = integrate.quad(lambda x: DthetaDr(x) , radial_dist[i], 5)
-	print i, ' : ', result
-	sol1.append(result[0])
+	sol1 = []
+	for i in range(len(radial_dist_1)):
+		result = integrate.quad(lambda x: DthetaDr(x,ray_param) , radial_dist_1[i], 5)
+		print 'calculating for ray parameter = ',ray_param,'::', i, ' : ', result 
+		sol1.append(result[0])
 
-sol2 = []
+	sol2 = []
+	for i in range(len(radial_dist_1)):
+		sol2.append(2*thetaC - sol1[i] )		
+
+	
+	return [radial_dist_1,sol1,sol2]
+
+ray_param_list = np.linspace(-4.95,4.95,20)
 
 ax = plt.subplot(111, projection='polar')    # add subplot in polar coordinates 
-ax.plot(sol1, radial_dist ,color='b', linewidth=3)
 ax.set_rmax(4)  
 ax.grid(True)  
-#ax.legend(loc='best')
 
+for i in range(len(ray_param_list)):
+	array = simulation(ray_param_list[i])
+	radial_dist = array[0]
+	sol1 = array[1]
+	sol2 = array[2]
+	
+	ax.plot(sol1,radial_dist,sol2,radial_dist ,color='b', linewidth=3)
 plt.show()
+
+
